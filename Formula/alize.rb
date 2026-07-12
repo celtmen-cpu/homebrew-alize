a0503eca83006ffe72b2ff5ec439f20d94e70d0e
celtmen <achintoutain.clement@icloud.com>
2026-07-12 13:45:08 +0200

    Update alize.rb

--- Formula/alize.rb
+++ Formula/alize.rb
@@ -1,17 +1,12 @@
 class Alize < Formula
   desc "Interpreted programming language Alizé"
   homepage "https://github.com/celtmen-cpu/Alize"
-  url "https://github.com/celtmen-cpu/Alize/releases/download/0.0.2/Alize-0.0.2.tar.gz"
+  url "https://github.com/celtmen-cpu/Alize/releases/download/v0.0.2/Alize-0.0.2.tar.gz"
   sha256 "SHA256_DE_L_ARCHIVE"
-  license "Freeware"
+  license "Proprietary"
 
   def install
-    libexec.install Dir["*"]
-
-    bin.write_exec_script <<~EOS
-      #!/bin/bash
-      python3 #{libexec}/main.py "$@"
-    EOS
+    bin.install "alize"
   end
 
   test do
