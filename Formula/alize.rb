8b06c5e660650c50e288cf9085302ac1cd854aa6
celtmen <achintoutain.clement@icloud.com>
2026-07-18 13:22:53 +0200

    Update alize.rb

--- Formula/alize.rb
+++ Formula/alize.rb
@@ -1,6 +1,6 @@
 class Alize < Formula
   desc "Interpreted programming language Alizé"
-  homepage "https://github.com/celtmen-cpu/alize-V2"
+  homepage "https://github.com/celtmen-cpu/homebrew-alize"
   url "https://github.com/celtmen-cpu/homebrew-alize/releases/download/0.0.6/alize-final.tar.gz"
   sha256 "c593b1fdc6bdc0689162253fb0ea153fe33f8a3cb753c83e53676ce3ea32e33e"
   license "MIT"
